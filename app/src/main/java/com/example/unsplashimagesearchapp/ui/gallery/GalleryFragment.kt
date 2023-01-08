45f92c4440baaea9c7b40312910051c9981f1890
Ledaro <fzelaskowski@wp.pl>
2023-01-08 14:31:25 +0100

    add navigation graph

---
+++ app/src/main/java/com/example/unsplashimagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.example.unsplashimagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.example.unsplashimagesearchapp.R
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+}
