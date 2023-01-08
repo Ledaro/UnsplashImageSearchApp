bb31bdd7a359085c67b8d72f5e99e605b173bfa0
Ledaro <fzelaskowski@wp.pl>
2023-01-08 14:54:47 +0100

    add Paging Source and Paging Data

--- app/src/main/java/com/example/unsplashimagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/unsplashimagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.unsplashimagesearchapp.ui.gallery
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.example.unsplashimagesearchapp.data.UnsplashRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,5 +12,20 @@ import javax.inject.Inject
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
     private val unsplashRepository: UnsplashRepository
-): ViewModel() {
+) : ViewModel() {
+
+    companion object {
+        private const val DEFAULT_QUERY = "dogs"
+    }
+
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { queryString ->
+        unsplashRepository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
+
 }
